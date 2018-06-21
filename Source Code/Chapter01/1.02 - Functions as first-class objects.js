487099ba05252a35de1e6aa6c1f89b9106c149f4
MrLss <18237115038@163.com>
2018-06-21 17:08:41 +0800

    1.3 函数的使用方式

--- Source Code/Chapter01/1.02 - Functions as first-class objects.js
+++ Source Code/Chapter01/1.02 - Functions as first-class objects.js
@@ -1,5 +1,5 @@
 /* An anonymous function, executed immediately. */
-
+/* 1.匿名函数用括号包起来2.后再跟一个括号；  实现匿名函数自执行 */
 (function() {
   var foo = 10;
   var bar = 2;
@@ -8,14 +8,14 @@
 
 
 /* An anonymous function with arguments. */
-
+/* 1.动态传值 2.减少中间量 */
 (function(foo, bar) {
   alert(foo * bar);
 })(10, 2);
 
 
 /* An anonymous function that returns a value. */
-
+/* 返回一个值，并赋值给一个变量 */
 var baz = (function(foo, bar) {
   return foo * bar;
 })(10, 2);
@@ -26,7 +26,7 @@ var baz = (function(foo, bar) {
 /* An anonymous function used as a closure. */
 
 var baz;
-
+/* ??? */
 (function() {
   var foo = 10;
   var bar = 2;
