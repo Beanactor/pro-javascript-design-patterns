ca48f7e8f40e8bc5fd4bb3a281890925d29febb3
MrLss <18237115038@163.com>
2018-06-21 15:40:33 +0800

    1.1 五种方式实现开始和停止动画功能

--- Source Code/Chapter01/1.01 - The flexibility of JavaScript.js
+++ Source Code/Chapter01/1.01 - The flexibility of JavaScript.js
@@ -1,5 +1,5 @@
 /* Start and stop animations using functions. */
-
+/* 用两个函数分别解决动画开始和停止的行为 */
 function startAnimation() {
   ...
 }
@@ -11,7 +11,7 @@ function stopAnimation() {
 
 
 /* Anim class. */
-
+/* 用构造器创建类，该类声明两个控制开始和停止的方法 */
 var Anim = function() {
   ...
 };
@@ -32,7 +32,7 @@ myAnim.stop();
 
 
 /* Anim class, with a slightly different syntax for declaring methods. */
-
+/* 用构造器创建类，该类声明两个控制开始和停止的方法，只是声明的方式不一样 */
 var Anim = function() { 
   ...
 };
@@ -48,7 +48,7 @@ Anim.prototype = {
 
 
 /* Add a method to the Function class that can be used to declare methods. */
-
+/* 函数原型上添加一个方法，该方法可以为函数类型的数据（构造函数Anim）动态声明方法 */
 Function.prototype.method = function(name, fn) {
   this.prototype[name] = fn;
 };
@@ -68,7 +68,7 @@ Anim.method('stop', function() {
 
 
 /* This version allows the calls to be chained. */
-
+/* 通过返回this的方法，实现链式编程 */
 Function.prototype.method = function(name, fn) {
     this.prototype[name] = fn;
     return this;
