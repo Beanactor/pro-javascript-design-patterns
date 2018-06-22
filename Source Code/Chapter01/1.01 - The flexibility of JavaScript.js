b308e2bedad20f6daff2da43ea53d30e74ffe3b4
MrLss <18237115038@163.com>
2018-06-22 08:54:41 +0800

    review first chapter

--- Source Code/Chapter01/1.01 - The flexibility of JavaScript.js
+++ Source Code/Chapter01/1.01 - The flexibility of JavaScript.js
@@ -32,7 +32,7 @@ myAnim.stop();
 
 
 /* Anim class, with a slightly different syntax for declaring methods. */
-/* 用构造器创建类，该类声明两个控制开始和停止的方法，只是声明的方式不一样 */
+/* 用构造器创建类，该类声明两个控制开始和停止的方法，只是声明的方式不一样，即把类的定义封装在一条声明中 */
 var Anim = function() { 
   ...
 };
@@ -48,7 +48,8 @@ Anim.prototype = {
 
 
 /* Add a method to the Function class that can be used to declare methods. */
-/* 函数原型上添加一个方法，该方法可以为函数类型的数据（构造函数Anim）动态声明方法 */
+/* 函数原型上添加一个方法，该方法可以为函数类型的数据（构造函数Anim）动态声明方法
+   因为Anim类继承自Function*/
 Function.prototype.method = function(name, fn) {
   this.prototype[name] = fn;
 };
